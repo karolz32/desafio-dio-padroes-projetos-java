8f931620ff7115c043aa43fd992afeee8fe6f19e
karolz32 <karoline.lima32@gmail.com>
2022-01-30 21:37:18 -0300

    add project

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.gof.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity @Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    private Endereco endereco;
+}
